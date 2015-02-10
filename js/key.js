3b1c6f9eabd2466251956149ef7c51bd142de5e1
thyhates <liaozhipu@sina.com>
2015-02-10 15:57:37 +0800

    key
    moblie

---
+++ js/key.js
@@ -0,0 +1,37 @@
+$(document).ready(function (e) {
+    $("#hide").click(function () {
+        $("#key").hide();
+    });
+    $(".word").click(function () {
+        $("#key").show();
+    });
+    var password = [];
+    $("td").each(function (i) {
+        $(".key" + i).click(function () {
+            for (var j = 0; j <= 5; j++) {
+                if (password[j] == null) {
+                    if (i == 10) {
+                        password[j] = 0;
+                    } else {
+                        password[j] = i + 1;
+                    }
+                    $(".text" + (j + 1)).css("display", "block");
+                    return false;
+                }
+            }
+        });
+    });
+    $(".reset").click(function () {
+        password = [];
+        $(".text").css("display", "none");
+    });
+    $(".back").click(function () {
+        for (var i = 5; i >= 0; i--) {
+            if (password[i] != null) {
+                password[i] = null;
+                $(".text" + (i + 1)).css("display", "none");
+                return false;
+            }
+        }
+    });
+});
